fa27a14dd0550a5d885f8f8f76813d58cfd4f87d
VitaliiZhukov <VitaliiZhukov>
2015-01-19 00:20:53 +0300

    Commitment #2

--- cachematrix.R
+++ cachematrix.R
@@ -8,17 +8,26 @@ makeCacheMatrix <- function(x = matrix()) {
     print("Matrix is not square! Try again.")
   }
   else {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+    ## m is NULL. 
+    m <- NULL
+    
+    ## set is the function that determines initial matrix for the makeCacheMatrix object. 
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    
+    ## get is the function that returns initial matrix
+    get <- function() x
+    ## setinverse is the function that assigns parameter matrix to m
+    setinverse <- function(solve) m <<- solve
+    ## getinverse is the function that returns m value
+    getinverse <- function() m
+    
+    ## Here is the list of inner functions
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
   }
 }
 
@@ -28,12 +37,19 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
+  
+  ## If the function is called NOT for the first time then m is not null and we get the already saved data 
   if(!is.null(m)) {
     message("getting cached data")
+    ##Returns m and quits the function running.
     return(m)
   }
+  
+  ## If the function is called for the first time then we get the initial matrix and call a solve function to it.
   data <- x$get()
   m <- solve(data, ...)
+  ## and set result as inverse value back to the makeCacheMatrix object.
   x$setinverse(m)
+  ## Print inverse matrix
   m
 }
